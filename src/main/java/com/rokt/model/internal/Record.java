70acd7e90da91859374b74551ddb54d23b6198e8
vikassharma.v <vikassharma.v@flipkart.com>
2022-02-16 19:40:57 +0530

    Added to strig to Record.java

--- src/main/java/com/rokt/model/internal/Record.java
+++ src/main/java/com/rokt/model/internal/Record.java
@@ -2,10 +2,12 @@ package com.rokt.model.internal;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 import org.joda.time.DateTime;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class Record {
     DateTime dateTime;
     String email;
